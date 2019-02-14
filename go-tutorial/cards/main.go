e39d52f06d0c1da43f87c0b3ea6a56dbd10583fe
本間裕大 <honmayudai@honmahiroshidainoMacBook-Pro.local>
2019-02-14 17:35:10 +0900

    refs none update

--- go-tutorial/cards/main.go
+++ go-tutorial/cards/main.go
@@ -1,11 +1,12 @@
 package main
 
 func main() {
-	// var card string = "Ace of Spades"
-	cards := deck{"Ace of Diamonds", newCard()}
-	cards = append(cards, "Six of Spades")
+	cards := newDeck()
 
-	cards.print()
+	hand, remainingCards := deal(cards, 5)
+	hand.print()
+	remainingCards.print()
+
 }
 
 func newCard() string {
